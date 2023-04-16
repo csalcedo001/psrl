8b058a199d9e8f1cf5102b850f0bd538d30779f4
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-04-16 00:03:28 +0000

    Implemented random MDP

--- psrl/envs/__init__.py
+++ psrl/envs/__init__.py
@@ -1,2 +1,4 @@
 from .gridworld import GridworldEnv, TwoRoomGridworldEnv, FourRoomGridworldEnv
+from .riverswim import RiverSwimEnv
+from .random_mdp import RandomMDPEnv
 from .utils import get_grid_from_file
--- psrl/envs/random_mdp.py
+++ psrl/envs/random_mdp.py
@@ -3,32 +3,57 @@ from gym import spaces
 import numpy as np
 
 class RandomMDPEnv(gym.Env):
-    def __init__(self, n_states, n_actions):
+    def __init__(self, n_states, n_actions, max_steps=10000):
         super().__init__()
+
         self.action_space = spaces.Discrete(n_actions)
         self.observation_space = spaces.Discrete(n_states)
 
-        self.n_states = n_states
-        self.n_actions = n_actions
+        self.max_steps = max_steps
 
         # For Diritchlet distribution
-        self.alpha = np.ones((n_states, n_actions, n_states))
+        self.alpha = np.ones((n_states)) * 1. / n_states
 
         self.transitions = None
+        self.rewards = None
+        self.state = None
+        self.steps = None
+
         self.randomize()
-
-        self.state = 0
-
         self.reset()
     
+
     def reset(self):
         self.state = 0
+        self.steps = 0
+
         return self.state
 
+
     def step(self, action):
-        self.state = np.random.randint(0, self.n_states)
-        return self.state, 0, False, {}
+        # Get next state
+        probs = self.transitions[self.state, action]
+        next_state = np.random.choice(self.observation_space.n, p=probs)
+
+        # Get reward
+        r_mean = self.r_mean[self.state, action, next_state]
+        r_var = self.r_var[self.state, action, next_state]
+        reward = np.random.normal(r_mean, r_var)
+
+        # Get termination condition
+        done = self.steps == self.max_steps - 1
+
+        # Updat state
+        self.state = next_state
+        self.steps += 1
+
+        return self.state, reward, done, {}
+
 
     def randomize(self):
-        transitions = np.random.dirichlet(self.alpha, size=(self.n_states, self.n_actions))
-        rewards = np.random.gamma(1, 1, size=(self.n_states, self.n_actions))
+        n_s = self.observation_space.n
+        n_a = self.action_space.n
+
+        self.transitions = np.random.dirichlet(self.alpha, size=(n_s, n_a))
+        self.r_var = np.random.gamma(1, 1, size=(n_s, n_a, n_s))
+        self.r_mean = np.random.normal(1, 1, size=(n_s, n_a, n_s))
