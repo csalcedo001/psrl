6e14c4ea9dc16648b8722c2c14e1da2aac680fd0
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-06-30 20:18:23 +0000

    Printed more descriptive information from transformer's output

--- experiments/2023-06-30/transformers/trajectory_transformer_quickstart.py
+++ experiments/2023-06-30/transformers/trajectory_transformer_quickstart.py
@@ -16,6 +16,11 @@ model.eval()
 
 observations_dim, action_dim, batch_size = 17, 6, 256
 seq_length = observations_dim + action_dim + 1
+vocab_size = model.config.vocab_size
+n_head = model.config.n_head
+hidden_size = model.config.n_embd
+embed_size_per_head = hidden_size / n_head
+num_hidden = model.config.n_layer
 
 trajectories = torch.LongTensor(np.array([np.random.permutation(seq_length) for _ in range(batch_size)])).to(
     device
@@ -33,8 +38,33 @@ outputs = model(
     return_dict=True,
 )
 
-print("Loss:", outputs.loss)
-print("Logits shape:", [t1.shape for t1 in outputs.logits])
-print("Past key values:", [[t2.shape for t2 in t1] for t1 in outputs.past_key_values])
-print("Hidden states shape:", [t1.shape for t1 in outputs.hidden_states])
-print("Attentions shape:", [t1.shape for t1 in outputs.attentions])
+
+loss = outputs.loss
+logits = outputs.logits
+past_key_values = past_key_values = torch.Tensor([[t2.cpu().detach().numpy().tolist() for t2 in t1] for t1 in outputs.past_key_values])
+hidden_states = torch.Tensor([t1.cpu().detach().numpy().tolist() for t1 in outputs.hidden_states])
+attentions = torch.Tensor([t1.cpu().detach().numpy().tolist() for t1 in outputs.attentions])
+
+
+# Single float
+print("Loss (scalar):")
+print("   ", loss)
+
+# Shape: (batch_size, sequence_length, vocab_size)
+print("Logits shape (batch_size, sequence_length, vocab_size):")
+print("   ", logits.shape)
+
+# For each of the n layers
+# Shape: (batch_size, num_heads, sequence_length, embed_size_per_head)
+print("Past key values shape (n_layers, ?, batch_size, num_heads, sequence_length, embed_size_per_head):")
+print("   ", past_key_values.shape)
+
+# For each hidden state
+# Shape: (batch_size, sequence_length, hidden_size)
+print("Hidden states shape (n_layers + 1, batch_size, sequence_length, hidden_size):")
+print("   ", hidden_states.shape)
+
+# For each hidden layer
+# Shape: (batch_size, num_heads, sequence_length, sequence_length)
+print("Attentions shape (n_layers, batch_size, num_heads, sequence_length, sequence_length):")
+print("   ", attentions.shape)
