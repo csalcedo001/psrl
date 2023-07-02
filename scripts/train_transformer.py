0e5bfdcdb1761091ed631eead0360e100fcd2e21
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-02 01:44:36 +0000

    Moved TrajectoryDataset to a module

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -11,46 +11,8 @@ from psrl.config import get_env_config
 from psrl.utils import env_name_map
 
 from arg_utils import get_experiment_parser
+from trajectory_dataset import TrajectoryDataset
 from utils import load_experiment_config, set_seed, get_file_path_from_config, get_experiment_path_from_config
-
-
-
-class TrajectoryDataset(Dataset):
-    def __init__(self, env, raw_trajectories, seq_len=64, beta=8):
-        if seq_len % 2 != 0:
-            raise ValueError("seq_len must be even")
-        
-        self.seq_len = seq_len
-        self.beta = beta
-        self.missing_token = env.observation_space.n + env.action_space.n
-        
-        trajectories = []
-        for s, a, _, _ in raw_trajectories:
-            trajectories.append(s + env.action_space.n)
-            trajectories.append(a)
-        
-        self.trajectories = trajectories
-    
-    def get_vocab_size(self):
-        return self.missing_token + 1
-
-    def __len__(self):
-        return (len(self.trajectories) - self.seq_len) // 2
-    
-    def __getitem__(self, idx):
-        # Get trajectory of length seq_len
-        trajectory = np.array(self.trajectories[2 * idx: 2 * idx + self.seq_len])
-
-        y = torch.LongTensor(trajectory)
-
-        # Mask some actions
-        missing_actions = 1 + min(int(np.random.exponential(self.beta)), self.seq_len // 2)
-        missing_s_and_a = 2 * missing_actions - 1
-
-        x = torch.LongTensor(trajectory.copy())
-        x[-missing_s_and_a:] = self.missing_token
-
-        return x, y
 
 
 
