8c664e89b3b16eea967fc1d4c560085cc05a5157
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-02 00:43:45 +0000

    Added script that generates data in parallel through multiprocessing with screen

---
+++ generate_regret_plot_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+envs=(riverswim tworoom fourroom)
+agents=(psrl ucrl2 klucrl)
+seeds=(0 1 2)
+
+for env in "${envs[@]}"
+do
+    for agent in "${agents[@]}"
+    do
+        for seed in "${seeds[@]}"
+        do
+            screen -dmS regret_${env}_${agent}_${seed} python3 scripts/generate_data.py \
+                --seed $seed \
+                --config configs/${env}_${agent}.yaml
+        done
+    done
+done
