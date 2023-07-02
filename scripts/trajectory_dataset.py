fe5033687e6b8b0bd351852fe8f62377fb1fee67
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-02 16:06:33 +0000

    Updated default beta for action masking

--- scripts/trajectory_dataset.py
+++ scripts/trajectory_dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 
 
 class TrajectoryDataset(Dataset):
-    def __init__(self, env, raw_trajectories, seq_len=64, beta=8):
+    def __init__(self, env, raw_trajectories, seq_len=64, beta=4):
         if seq_len % 2 != 0:
             raise ValueError("seq_len must be even")
         
