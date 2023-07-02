79f1fe0fa46c92869e5934a82205dd0e00f7cd0c
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-02 02:07:42 +0000

    Make it possible to plot for psrl (only some plots so far)

---
+++ scripts/eval_transformer.py
@@ -0,0 +1,158 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from transformers import GPT2Config, GPT2LMHeadModel
+import pickle
+from tqdm import tqdm
+from accelerate import Accelerator
+
+from psrl.config import get_env_config
+from psrl.utils import env_name_map
+
+from arg_utils import get_experiment_parser
+from trajectory_dataset import TrajectoryDataset
+from utils import load_experiment_config, set_seed, get_file_path_from_config, get_experiment_path_from_config
+
+
+
+
+
+
+# Get experiment configuration
+parser = get_experiment_parser()
+args = parser.parse_args()
+config_path = args.config
+exp_config = load_experiment_config(config_path)
+
+
+
+# Setup experiment
+seed = exp_config.seed
+if args.seed is not None:
+    seed = args.seed
+set_seed(seed)
+print("*** SEED:", seed)
+
+no_goal = exp_config.no_goal
+if args.goal_reward is not None:
+    no_goal = args.goal_reward == 0
+    
+if not no_goal:
+    exp_config.save_path = os.path.join(exp_config.save_path, 'regret_plot')
+    exp_config.plots_path = os.path.join(exp_config.plots_path, 'regret_plot')
+
+data_dir = get_experiment_path_from_config(exp_config, mkdir=True, root_type='data')
+accelerator = Accelerator(project_dir=data_dir)
+
+
+
+# Get environment
+env_class = env_name_map[exp_config.env]
+env_config = get_env_config(exp_config.env)
+env_config['gamma'] = exp_config.gamma
+env_config['no_goal'] = no_goal
+env = env_class(env_config)
+
+
+
+# Get dataset of trajectories
+checkpoints_path = os.path.join(os.path.dirname(__file__), exp_config.save_path)
+os.makedirs(checkpoints_path, exist_ok=True)
+
+trajectories_path = get_file_path_from_config('trajectories.pkl', exp_config)
+with open(trajectories_path, 'rb') as f:
+    raw_trajectories = pickle.load(f)
+
+trajectory_dataset = TrajectoryDataset(
+    env,
+    raw_trajectories,
+    seq_len=exp_config.seq_len
+)
+vocab_size = trajectory_dataset.get_vocab_size()
+
+data_loader = DataLoader(
+    trajectory_dataset,
+    batch_size=exp_config.batch_size,
+    shuffle=True,
+    drop_last=True,
+)
+
+
+
+# Get model
+model_config = GPT2Config()
+
+model_config.vocab_size = vocab_size
+model_config.n_positions = exp_config.seq_len
+model_config.n_ctx = exp_config.seq_len
+
+model = GPT2LMHeadModel(model_config)
+model.train()
+
+
+
+# Training
+optimizer = torch.optim.Adam(model.parameters(), lr=exp_config.lr)
+criterion = torch.nn.CrossEntropyLoss()
+
+model, optimizer, data_loader = accelerator.prepare(model, optimizer, data_loader)
+
+losses = []
+
+print("Starting training...")
+for epoch in range(exp_config.epochs):
+    pbar = tqdm(total=len(data_loader))
+    for batch in data_loader:
+        x, y = batch
+        
+        output = model(input_ids=x)
+        y_hat = output.logits.view(-1, vocab_size)
+        loss = criterion(y_hat, y.view(-1))
+
+        accelerator.backward(loss)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+        losses.append(loss.item())
+
+        pbar.update(1)
+        pbar.set_description(f"[{epoch}/{exp_config.epochs}] Loss: {loss.item():.4f}")
+    
+    if epoch % 10 == 0:
+        checkpoints_dir = get_file_path_from_config('checkpoints', exp_config)
+        accelerator.save_state(checkpoints_dir)
+
+
+
+# Evaluation after training
+device = accelerator.device
+
+x, y = next(iter(data_loader))
+x = x.to(device)
+
+model.eval()
+output = model(input_ids=x)
+y_hat_post = output.logits.argmax(dim=-1).cpu().numpy()
+
+print("x:")
+print(x.cpu().detach().numpy()[-10:, -20:])
+print()
+
+print("y_hat:")
+print(y_hat_post[-10:, -20:])
+print()
+
+print("y:")
+print(y.cpu().numpy()[-10:, -20:])
+print()
+
+
+
+# Save results
+model_path = get_file_path_from_config('model.pt', exp_config, mkdir=True)
+torch.save(model.state_dict(), model_path)
+
+losses_path = get_file_path_from_config('losses.pkl', exp_config, mkdir=True)
+with open(losses_path, 'wb') as f:
+    pickle.dump(losses, f)
--- scripts/plot_agent.py
+++ scripts/plot_agent.py
@@ -1,4 +1,6 @@
 import os
+import torch
+import torch.distributions as dist
 import numpy as np
 
 from psrl.config import get_env_config, get_agent_config
@@ -70,6 +72,32 @@ if exp_config.agent in ['ucrl2', 'kl_ucrl']:
     v = agent.u
     q = agent.q
     state_visitation = np.sum(agent.Nk + agent.vk, axis=1)
+elif exp_config.agent in ['psrl']:
+    samples = 10
+
+    p_dist = agent.p_dist
+    mu0, lambd, alpha, beta = agent.r_dist
+
+    p_hats = []
+    r_hats = []
+    for _ in range(samples):
+        p_hat = dist.Dirichlet(p_dist).sample()
+
+        tau = dist.Gamma(alpha, 1. / beta).sample()
+        mu = dist.Normal(mu0, 1. / torch.sqrt(lambd * tau)).sample()
+        r_hat = mu
+
+        p_hats.append(p_hat)
+        r_hats.append(r_hat)
+
+    p_hat = torch.stack(p_hats).mean(dim=0).numpy()
+    r_hat = torch.stack(r_hats).mean(dim=0).numpy()
+    p_count = np.array(agent.p_count)
+    r_count = np.array(agent.r_total)
+
+    v = None
+    q = None
+    state_visitation = None
 else:
     raise Exception(f'Agent {exp_config.agent} not supported.')
 
@@ -84,31 +112,33 @@ save_policy_plot(
     get_file_path_from_config('policy.png', exp_config, mkdir=True, root_type='plots'),
     title='Policy',
 )
-
 save_expected_reward_heatmap_plot(
     env,
     r_hat,
     get_file_path_from_config('expected_reward.png', exp_config, root_type='plots'),
     title='Expected Reward',
 )
-save_action_value_heatmap_plot(
-    env,
-    q,
-    get_file_path_from_config('action_value_function.png', exp_config, root_type='plots'),
-    title='Action Value Function',
-)
-save_state_value_heatmap_plot(
-    env,
-    v,
-    get_file_path_from_config('state_value.png', exp_config, root_type='plots'),
-    title='State Value Function',
-)
-save_empirical_state_visitation_heatmap_plot(
-    env,
-    state_visitation,
-    get_file_path_from_config('empirical_state_visitation.png', exp_config, root_type='plots'),
-    title='Empirical State Visitation',
-)
+if q is not None:
+    save_action_value_heatmap_plot(
+        env,
+        q,
+        get_file_path_from_config('action_value_function.png', exp_config, root_type='plots'),
+        title='Action Value Function',
+    )
+if v is not None:
+    save_state_value_heatmap_plot(
+        env,
+        v,
+        get_file_path_from_config('state_value.png', exp_config, root_type='plots'),
+        title='State Value Function',
+    )
+if state_visitation is not None:
+    save_empirical_state_visitation_heatmap_plot(
+        env,
+        state_visitation,
+        get_file_path_from_config('empirical_state_visitation.png', exp_config, root_type='plots'),
+        title='Empirical State Visitation',
+    )
 save_reward_count_heatmap_plot(
     env,
     r_count,
