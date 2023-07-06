28bf3739f08616b70aa0dbe9cc5c5c2ad17aa2ae
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-06 01:21:29 +0000

    Updated plot_metrics.py to new setup

--- scripts/plot_metrics.py
+++ scripts/plot_metrics.py
@@ -1,32 +1,21 @@
-import os
 import pickle
 
+from setup_script import setup_script
+from file_system import load_pickle
 from plotting import save_losses_plot, save_accuracy_plot
-from arg_utils import get_experiment_parser, process_experiment_config
-from utils import load_experiment_config, set_seed, get_file_path_from_config
+from utils import get_file_path_from_config
 
 
 
 
-# Get experiment configuration
-parser = get_experiment_parser()
-args = parser.parse_args()
-config_path = args.config
-exp_config = load_experiment_config(config_path)
-exp_config = process_experiment_config(args, exp_config)
-
-
-
-# Setup experiment
-set_seed(exp_config.seed)
-print("*** SEED:", exp_config.seed)
+# Setup script
+exp_config, env, _, accelerator = setup_script()
 
 
 
 # Get losses data
 metrics_path = get_file_path_from_config('metrics.pkl', exp_config)
-with open(metrics_path, 'rb') as f:
-    metrics = pickle.load(f)
+metrics = load_pickle(metrics_path)
 
 
 
