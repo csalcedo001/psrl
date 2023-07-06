4bf615373dcaec7b1fd48762a6d9dafd72e872cb
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-06 19:53:02 +0000

    Corrected computation of validation loss

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -5,6 +5,7 @@ def compute_metrics(model, data_loader, criterion):
     vocab_size = data_loader.dataset.get_vocab_size()
 
     loss = 0
+    total_batch_loss = 0
     hits = 0
     hits_and_misses = 0
     last_action_hits = 0
@@ -16,7 +17,8 @@ def compute_metrics(model, data_loader, criterion):
         output = model(input_ids=x)
         y_logits = output.logits.view(-1, vocab_size)
         batch_loss = criterion(y_logits, y.view(-1))
-        loss = batch_loss.item() / (i + 1)
+        total_batch_loss += batch_loss.item()
+        loss = total_batch_loss / (i + 1)
 
         # Compute accuracy
         y_hat = output.logits.argmax(dim=-1)
