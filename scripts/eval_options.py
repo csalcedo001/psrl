73cd308a5dfa267b4f80797bbc0208661a96a72a
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-06 01:15:53 +0000

    Updated eval_options.py to current setup

--- scripts/eval_options.py
+++ scripts/eval_options.py
@@ -1,49 +1,18 @@
-import os
-import torch
-from torch.utils.data import DataLoader
 from transformers import GPT2Config, GPT2LMHeadModel
-import pickle
-from tqdm import tqdm
-from accelerate import Accelerator
 
 from psrl.agents import RandomAgent
-from psrl.config import get_env_config, get_agent_config
-from psrl.utils import env_name_map, agent_name_map
 from psrl.rollout import rollout
 
-from arg_utils import get_experiment_parser, process_experiment_config
+from setup_script import setup_script
+from file_system import save_pickle
 from options import OptionEnvWrapper
-from utils import load_experiment_config, set_seed, get_file_path_from_config, get_experiment_path_from_config
+from utils import get_file_path_from_config
 
 
 
 
-
-
-# Get experiment configuration
-parser = get_experiment_parser()
-args = parser.parse_args()
-config_path = args.config
-exp_config = load_experiment_config(config_path)
-exp_config = process_experiment_config(args, exp_config)
-
-
-
-# Setup experiment
-set_seed(exp_config.seed)
-print("*** SEED:", exp_config.seed)
-
-data_dir = get_experiment_path_from_config(exp_config, mkdir=True, root_type='data')
-accelerator = Accelerator(project_dir=data_dir)
-
-
-
-# Get environment
-env_class = env_name_map[exp_config.env]
-env_config = get_env_config(exp_config.env)
-env_config['gamma'] = exp_config.gamma
-env_config['no_goal'] = False
-env = env_class(env_config)
+# Setup script
+exp_config, env, _, accelerator = setup_script()
 
 
 
@@ -104,5 +73,4 @@ print("Total env reward:", total_env_reward)
 
 # Save trajectories
 trajectories_path = get_file_path_from_config('option_trajectories.pkl', exp_config, mkdir=True)
-with open(trajectories_path, 'wb') as f:
-    pickle.dump(trajectory, f)
+save_pickle(trajectory, trajectories_path)
