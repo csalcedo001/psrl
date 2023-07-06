9eb6ac6acdad0dc607b0384a9a3bd86a3b0b7b4b
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-06 01:19:36 +0000

    Updated plot_agent.py to current setup

--- scripts/plot_agent.py
+++ scripts/plot_agent.py
@@ -1,11 +1,8 @@
-import os
 import torch
 import torch.distributions as dist
 import numpy as np
 
-from psrl.config import get_env_config, get_agent_config
-from psrl.utils import env_name_map, agent_name_map
-
+from setup_script import setup_script
 from plotting import (
     save_policy_plot,
     save_expected_reward_heatmap_plot,
@@ -14,40 +11,13 @@ from plotting import (
     save_empirical_state_visitation_heatmap_plot,
     save_reward_count_heatmap_plot,
 )
-from arg_utils import get_experiment_parser, process_experiment_config
-from utils import load_experiment_config, set_seed, get_file_path_from_config
+from utils import get_file_path_from_config
 
 
 
 
-# Get experiment configuration
-parser = get_experiment_parser()
-args = parser.parse_args()
-config_path = args.config
-exp_config = load_experiment_config(config_path)
-exp_config = process_experiment_config(args, exp_config)
-
-
-
-# Setup experiment
-set_seed(exp_config.seed)
-print("*** SEED:", exp_config.seed)
-
-
-
-# Get environment
-env_class = env_name_map[exp_config.env]
-env_config = get_env_config(exp_config.env)
-env_config['gamma'] = exp_config.gamma
-env_config['no_goal'] = exp_config.no_goal
-env = env_class(env_config)
-
-
-
-# Get agent
-agent_class = agent_name_map[exp_config.agent]
-agent_config = get_agent_config(exp_config.agent)
-agent = agent_class(env, agent_config)
+# Setup script
+exp_config, env, agent, accelerator = setup_script()
 agent_path = get_file_path_from_config('agent.pkl', exp_config)
 agent.load(agent_path)
 
