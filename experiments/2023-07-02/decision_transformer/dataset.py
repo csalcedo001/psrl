f789988cd51c61d9dfffc74c09b3a85a42360bc0
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2023-07-07 22:22:02 +0000

    Success in training decision transformer

--- experiments/2023-07-02/decision_transformer/dataset.py
+++ experiments/2023-07-02/decision_transformer/dataset.py
@@ -4,4 +4,7 @@ import numpy as np
 dataset = load_dataset("edbeeching/decision_transformer_gym_replay", "halfcheetah-expert-v2")
 
 print(dataset)
-print(np.array(dataset['train']['observations']).shape)
+print("Observations shape:", np.array(dataset['train']['observations']).shape)
+print("Actions shape:     ", np.array(dataset['train']['actions']).shape)
+print("Rewards shape:     ", np.array(dataset['train']['rewards']).shape)
+print("Dones shape:       ", np.array(dataset['train']['dones']).shape)
